a6a61ff0b4d3a4d8abbe72997e39cb7cd4121d91
IronVeil <git@ironveil.cloud>
2023-08-07 16:26:37 +0100

    Created index page & SCSS

---
+++ pages/index.jsx
@@ -0,0 +1,50 @@
+// --- Index Page ---
+
+// Import modules
+import { useRouter } from "next/router"
+import { useCookies } from "react-cookie"
+import useSWR from 'swr'
+
+// Import styling
+import styles from "./index.module.scss"
+
+// API fetcher
+const fetcher = (url, token) => fetch(url, { headers: { "Authorization": token } }).then((res) => res.json())
+
+// Index page
+export default function Index() {
+
+    // Get the token from cookies
+    const cookies = useCookies()
+    const token = cookies.token
+
+    // Alow page navigation
+    const router = useRouter()
+
+    // Check if the login token is valid
+    const { data, error, isLoading } = useSWR([ "/api/checklogin", token ], () => fetcher("/api/checklogin", token))
+
+    // If there is an error in loading the token
+    if (error) return (
+        <div className={styles.error + " " + styles.main}>
+            <div>
+                <p>Error</p>
+                <p>{error}</p>
+            </div>
+        </div>
+    )
+
+    // While loading
+    if (isLoading) return (
+        <div className={styles.loading + " " + styles.main}>
+            <img src="/logo.png" />
+        </div>
+    )
+
+    // Navigate based on token valid-ness
+    if (data == true) {
+        router.push("/dashboard")
+    } else {
+        router.push("/login")
+    }
+}
