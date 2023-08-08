45f66fc387ae02ceae31ac050f76f0b25e6d4174
IronVeil <git@ironveil.cloud>
2023-08-08 10:19:39 +0100

    Added generic tab title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,14 +5,21 @@ import { CookiesProvider } from "react-cookie"
 
 // Import global styling
 import "./app.scss"
+import Head from "next/head"
 
 // Global page wrapper
 export default function Cardify({ Component, pageProps }) {
 
     // Return rendered component
     return (
-        <CookiesProvider>
-            <Component {...pageProps} />
-        </CookiesProvider>
+        <>
+            <Head>
+                <title>Cardify</title>
+            </Head>
+
+            <CookiesProvider>
+                <Component {...pageProps} />
+            </CookiesProvider>
+        </>
     )
 }
