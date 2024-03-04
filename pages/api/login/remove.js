c77c9d6a38f6b3e799c8a731cdf372a27f8df13d
IronVeil <git@ironveil.cloud>
2024-03-04 19:56:45 +0000

    Updated signout API route.

--- pages/api/login/remove.js
+++ pages/api/login/remove.js
@@ -3,21 +3,22 @@
 // Import Prisma DB
 import prisma from "../../../lib/prisma"
 
-// /api/login/check 
+// /api/login/remove 
 export default function handler(req, res) {
 
     // Get token from header
     let token = req.headers.authorization
 
-    // If no token, return false
+    // If no token, return 401
     if (token == "undefined") {
 
-        res.send("false")
+        res.send(401)
 
     } else {
 
         // Try and delete the token
         prisma.token.delete({
+            
             where: {
                 id: token
             }
